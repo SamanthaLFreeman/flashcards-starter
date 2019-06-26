3f7c04a9b0e7972fcce4ca1912ae107994334654
Alan Turing <freemasl.sf@gmail.com>
2019-06-25 20:22:22 -0600

    Create a Round class and tests & add id to Card class

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,6 @@
 class Card {
   constructor(index, question, answers, correctAnswer) {
+    this.index = index;
     this.question = question;
     this.answers = answers;
     this.correctAnswer = correctAnswer;
--- test/Card-test.js
+++ test/Card-test.js
@@ -14,7 +14,12 @@ describe('Card', function() {
   it('should be an instance of Card', function() {
     const card = new Card();
     expect(card).to.be.an.instanceof(Card);
-  }); 
+  });
+
+  it('should store an index', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    expect(card.index).to.equal(1);
+  })
 
   it('should store a question', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
